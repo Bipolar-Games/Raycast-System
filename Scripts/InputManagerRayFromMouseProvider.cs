ef2ca0a29df32a515ac552844c2f934a3a953612
Michal <malpa.w.klapkach@gmail.com>
2023-07-05 15:15:50 +0200

    Create Raycast System with all classes needed

---
+++ Scripts/InputManagerRayFromMouseProvider.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Bipolar.RaycastSystem
+{
+#if ENABLE_LEGACY_INPUT_MANAGER
+    public class InputManagerRayFromMouseProvider : RayFromMouseProvider
+    {
+        protected override Vector2 GetScreenPosition()
+        {
+            return Input.mousePosition;
+        }
+    }
+#endif
+}
---
+++ Scripts/InputSystemRayFromMouseProvider.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Bipolar.RaycastSystem
+{
+#if ENABLE_INPUT_SYSTEM
+    public class InputSystemRayFromMouseProvider : RayFromMouseProvider
+    {
+        protected override Vector2 GetScreenPosition()
+        {
+            return Mouse.current.position.ReadValue();
+        }
+    }
+#endif
+}
